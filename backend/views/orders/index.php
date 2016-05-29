e44cf2e8a7a59e75a8ab47d5a16d690cbbe47982
Nazar Salabay <best.nazar@gmail.com>
2016-05-29 20:18:03 +0300

    CRUD Country

---
+++ backend/views/orders/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\OrdersSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Замовлення';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="orders-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Orders', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+<?php Pjax::begin(); ?>    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'created_at',
+            'customer_name',
+            'customer_phone',
+             'customer_email:email',
+             'message',
+             'selected_product',
+             'order_status',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+<?php Pjax::end(); ?></div>
